c5fcd21227d84dfa462229d9f0480f19815facab
Rodgers Andati <andatirodgers@gmail.com>
2019-01-25 14:12:27 +0300

    initial code - spring initializr

---
+++ src/main/java/com/andati/imbank/ImbankApplication.java
@@ -0,0 +1,14 @@
+package com.andati.imbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImbankApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/andati/imbank/ImbankApplicationTests.java
@@ -0,0 +1,17 @@
+package com.andati.imbank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImbankApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
